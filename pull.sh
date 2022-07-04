49478ca952ba3855aa758040b62c12666b95f28b
jcamilov <vargasjcamilo@gmail.com>
2022-07-04 16:08:43 -0500

    lun 04 jul 2022 16:08:43 -05

--- pull.sh
+++ pull.sh
@@ -1,3 +1,4 @@
 # solo para hacer el pull mas rápido sin estar escribiendo la dirección del commit.
 git pull https://github.com/jcamilov/coraltracker.git
 cd gstreamer
+python3 detect.py --tracker sort
